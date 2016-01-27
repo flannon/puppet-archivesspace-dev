cb1ca04527d6ea03f0e9d06562d78f899870ea63
Flannon Jackson <flannon@nyu.edu>
2016-01-27 17:21:16 -0500

    Changed creates to unless and put the require in file call.

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,53 +7,61 @@ class archivesspace_dev::service {
   exec { 'build/run backend:devserver' :
     cwd      => '/opt/archivesspace',
     command  => '/opt/archivesspace/build/run  backend:devserver',
-    creates   => "${install_dir}/.devserver_backend_started",
+    #creates   => "${install_dir}/.devserver_backend_started",
+    unless   => "/usr/bin/test -f ${::install_dir}/.devserver_backend_started",
     timeout  => 1800,
-    before   => File["${install_dir}/.devserver_backend_started"],
+    #before   => File["${install_dir}/.devserver_backend_started"],
   }
 
-  file { "${install_dir}/.devserver_backend_started]":
+  file { "${::install_dir}/.devserver_backend_started]":
     ensure  => present,
     content => 'aspace backend devserver started',
+    require => Exec['build/run backend:devserver'],
   }
 
   exec { 'build/run frontend:devserver' :
     command  => '/opt/archivesspace/build/run  frontend:devserver',
     cwd      => '/opt/archivesspace',
-    creates   => "${install_dir}/.devserver_frontend_started",
+    #creates   => "${install_dir}/.devserver_frontend_started",
+    unless   => "/usr/bin/test -f ${::install_dir}/.devserver_frontend_started",
     timeout  => 1800,
-    before   => File["${install_dir}/.devserver_frontend_started"],
+    #before   => File["${install_dir}/.devserver_frontend_started"],
   }
 
-  file { "${install_dir}/._devserver_frontend_started]":
+  file { "${::install_dir}/._devserver_frontend_started]":
     ensure  => present,
     content => 'aspace frontend devserver started',
+    require => Exec['build/run frontend:devserver'],
   }
 
   exec { 'build/run public:devserver' :
     command  => '/opt/archivesspace/build/run  public:devserver',
     cwd      => '/opt/archivesspace',
-    creates   => "${install_dir}/.devserver_public_started]",
+    #creates   => "${install_dir}/.devserver_public_started]",
+    unless   => "/usr/bin/test -f ${::install_dir}/.devserver_public_started]",
     timeout  => 1800,
-    before   => File["${install_dir}/.devserver_public_started"],
+    #before   => File["${install_dir}/.devserver_public_started"],
   }
 
-  file { "${install_dir}/._devserver_public_started]":
+  file { "${::install_dir}/._devserver_public_started]":
     ensure  => present,
     content => 'aspace public devserver started',
+    require => Exec['build/run public:devserver'],
   }
 
   exec { 'build/run indexer' :
     command  => '/opt/archivesspace/build/run  indexer',
     cwd      => '/opt/archivesspace',
-    creates   => "${install_dir}/.devserver_indexer_started]",
+    #creates   => "${install_dir}/.devserver_indexer_started]",
+    unless   => "/usr/bin/test -f ${::install_dir}/.devserver_indexer_started]",
     timeout  => 1800,
-    before   => File["${install_dir}/.devserver_indexer_started"],
+    #before   => File["${install_dir}/.devserver_indexer_started"],
   }
 
-  file { "${install_dir}/._devserver_indexer_started]":
+  file { "${::install_dir}/._devserver_indexer_started]":
     ensure  => present,
     content => 'aspace indexer devserver started',
+    require => Exec['build/run indexer'],
   }
 
 }
