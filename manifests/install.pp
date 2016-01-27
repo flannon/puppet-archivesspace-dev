e224b97ed08317d300d036696819bcd06f7fdee8
Flannon Jackson <flannon@nyu.edu>
2016-01-27 17:58:38 -0500

    Put the parameters in the install class.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,9 @@
-class archivesspace_dev::install 
-# (
-#$revision = 'master',
-#$source = 'https://github.com/NYULibraries/archivesspace.git'
-#) 
-{
+class archivesspace_dev::install (
+  $revision = archivesspace_dev::params::revision,
+  #$revision = 'master',
+  $source = archivesspace_dev::params::source,
+  #$source = 'https://github.com/NYULibraries/archivesspace.git'
+) inherits archivesspace_dev::params {
     vcsrepo { '/opt/archivesspace':
       ensure   => present,
       owner    => 'ec2-user',
