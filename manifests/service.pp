49e8c4333ace76a28a3705d44655c3b1ef934d7a
Flannon Jackson <flannon@5eight5.com>
2016-01-28 15:17:26 -0500

    Detached the server processes so they acutally run somewhat like daemons.

--- manifests/service.pp
+++ manifests/service.pp
@@ -9,14 +9,14 @@ class archivesspace_dev::service (
 
   exec { 'build/run backend:devserver' :
     cwd     => "${install_dir}",
-    command => "${install_dir}/build/run  backend:devserver",
+    command => "${install_dir}/build/run  backend:devserver &",
     creates => "${install_dir}/.devserver_backend_started",
     timeout => 1800,
     notify  => File [ "${install_dir}/.devserver_backend_started"],
     require => Vcsrepo["${install_dir}"],
   }
 
-  file { "${install_dir}/.devserver_backend_started]":
+  file { "${install_dir}/.devserver_backend_started":
     ensure  => present,
     content => 'aspace backend devserver started',
     owner   => "${user}",
@@ -25,14 +25,14 @@ class archivesspace_dev::service (
 
   exec { 'build/run frontend:devserver' :
     cwd     => "${install_dir}",
-    command => "${install_dir}/build/run  frontend:devserver",
+    command => "${install_dir}/build/run  frontend:devserver &",
     creates => "${install_dir}/.devserver_frontend_started",
     timeout => 1800,
     notify  => File [ "${install_dir}/.devserver_frontend_started"],
     require => Vcsrepo["${install_dir}"],
   }
 
-  file { "${::install_dir}/.devserver_frontend_started]":
+  file { "${install_dir}/.devserver_frontend_started":
     ensure  => present,
     content => 'aspace frontend devserver started',
     owner   => "${user}",
@@ -41,30 +41,30 @@ class archivesspace_dev::service (
 
   exec { 'build/run public:devserver' :
     cwd     => "${install_dir}",
-    command => "${install_dir}/build/run  public:devserver",
+    command => "${install_dir}/build/run  public:devserver &",
     creates => "${install_dir}/.devserver_public_started",
     timeout => 1800,
     notify  => File [ "${install_dir}/.devserver_public_started"],
     require => Vcsrepo["${install_dir}"],
   }
 
-  file { "${::install_dir}/.devserver_public_started]":
+  file { "${install_dir}/.devserver_public_started":
     ensure  => present,
-    content => 'aspace pulic devserver started',
+    content => 'aspace public devserver started',
     owner   => "${user}",
     group   => "${user}",
   }
 
   exec { 'build/run indexer' :
     cwd     => "${install_dir}",
-    command => "${install_dir}/build/run  indexer",
+    command => "${install_dir}/build/run  indexer &",
     creates => "${install_dir}/.devserver_indexer_started",
     timeout => 1800,
     notify  => File [ "${install_dir}/.devserver_indexer_started"],
     require => Vcsrepo["${install_dir}"],
   }
 
-  file { "${::install_dir}/.devserver_indexer_started]":
+  file { "${install_dir}/.devserver_indexer_started":
     ensure  => present,
     content => 'aspace indexer devserver started',
     owner   => "${user}",
