f660bb6c8c624f2c021c1c4b172c50f86fc944a8
Flannon Jackson <flannon@5eight5.com>
2016-01-28 19:27:43 -0500

    ...

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,17 @@
 class archivesspace_dev (
   $install_dir = archivesspace_dev::params::install_dir,
   $revision = archivesspace_dev::params::revision,
-  #$revision = 'master',
   $source = archivesspace_dev::params::source,
-  #$source = 'https://github.com/NYULibraries/archivesspace.git'
   $user = archivesspace_dev::params::user,
   ) inherits archivesspace_dev::params {
     #) {
-    #anchor { 'archivesspace_dev::begin': } ->
-    #include archivesspace_dev::install  
-    class { 'archivesspace_dev::install' :
-      revision => 'v1.4.2',
-      source   => 'https://github.com/NYULibraries/archivesspace.git',
-    }
+    include archivesspace_dev::install
+    #class { 'archivesspace_dev::install' :
+    #  revision => 'master',
+    #  source   => 'https://github.com/NYULibraries/archivesspace.git',
+    #}
     #include archivesspace_dev::bootstrap
     #include archivesspace_dev::service
-    #anchore { 'archivesspace_dev::end': }
-    #Class['archivesspace_dev::install'] -> Class ['archivesspace_dev::bootstrap']
+    #Class['archivesspace_dev::install'] -> Class ['archivesspace_dev::bootstrap'] -> Class['archivesspace_dev::service']
 
 }
