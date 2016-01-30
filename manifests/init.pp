c58e3cb15cb2e9ed38752045c249a8707b100f86
Flannon Jackson <flannon@5eight5.com>
2016-01-29 20:42:33 -0500

    moved all functionality for the module back to the init class.  Got rid of the service class, it's too problematic because it not really a service.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,14 +4,36 @@ class archivesspace_dev (
   $source = archivesspace_dev::params::source,
   $user = archivesspace_dev::params::user,
   ) inherits archivesspace_dev::params {
-    #) {
-    #include archivesspace_dev::install
-    class { 'archivesspace_dev::install' :
-      revision => 'master',
-      source   => 'https://github.com/NYULibraries/archivesspace.git',
-    }
-    include archivesspace_dev::bootstrap
-    include archivesspace_dev::service
-    Class['archivesspace_dev::install'] -> Class ['archivesspace_dev::bootstrap'] -> Class['archivesspace_dev::service']
+  │ #) {
+  │ #include archivesspace_dev::install
+  │ vcsrepo { $install_dir:
+  │ │ ensure   => present,
+  │ │ owner    => $user,
+  │ │ group    => $user,
+  │ │ provider => git,
+  │ │ require  => Package[ 'git' ],
+  │ │ source   => $source,
+  │ │ revision => $revision,
+  │ } 
+
+  │ exec { 'build/run bootstrap' :
+  │ │ cwd      => "${install_dir}",
+  │ │ command  => "${install_dir}/build/run bootstrap",
+  │ │ timeout  => 1800,
+  │ │ creates  => "${install_dir}/.devserver_bootstrap_has_run",
+  │ │ notify   => File [ "${install_dir}/.devserver_bootstrap_has_run/" ],
+  │ │ require  => Vcsrepo["${install_dir}"],
+  │ }
+
+  │ $msg = "    This file is a marker to stop puppet Exec from
+  │ re-running build/run bootstrap. Puppet will not re-run
+  │ build/run bootstrap if this file exists."
+
+  │ file { "${install_dir}/.devserver_bootstrap_has_run":
+  │ │ ensure  => present,
+  │ │ content => $msg,
+  │ │ owner   => $user,
+  │ │ group   => $user,
+  │ }
 
 }
