fcee4ede8059d1a984598c614dc833112b602397
Flannon <flannon@nyu.edu>
2016-02-10 17:16:55 -0500

    added an Exec for build/run db:migrate and a define remote_file to load the mysql java connector.

--- files/config.rb
+++ files/config.rb
@@ -33,7 +33,7 @@
 #AppConfig[:allow_other_unmapped] = false
 #
 #AppConfig[:db_url] = proc { AppConfig.demo_db_url }
-AppConfig[:db_url] = "jdbc:mysql://localhost:3306/asdb?user=as&password=vagrant&useUnicode=true&characterEncoding=UTF-8"
+AppConfig[:db_url] = "jdbc:mysql://localhost:3306/asdb?user=as&password=letmein&useUnicode=true&characterEncoding=UTF-8"
 #AppConfig[:db_url_redacted] = proc { AppConfig[:db_url].gsub(/(user|password)=(.*?)&/, '\1=[REDACTED]&') }
 #AppConfig[:db_max_connections] = proc { 20 + (AppConfig[:indexer_thread_count] * 2) }
 #
--- manifests/init.pp
+++ manifests/init.pp
@@ -13,23 +13,52 @@ class archivesspace_dev (
       source   => $source,
       revision => $revision,
     } 
+    remote_file{"${install_dir}/common/lib/mysql-connector-java-5.1.34.jar":
+    #remote_file{"/tmp/mysql-connector-java-5.1.34.jar":
+      remote_location => 'http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.34/mysql-connector-java-5.1.34.jar',
+      require         => Vcsrepo["$install_dir"],
+    }
     exec { 'build/run bootstrap' :
-      cwd      => "${install_dir}",
+      cwd      => $install_dir,
       command  => "${install_dir}/build/run bootstrap",
       timeout  => 1800,
-      creates  => "${install_dir}/.devserver_bootstrap_has_run",
-      notify   => File [ "${install_dir}/.devserver_bootstrap_has_run/" ],
-      require  => Vcsrepo["${install_dir}"],
+      creates  => "${install_dir}/.devserver_bootstrap_complete",
+      notify   => File["${install_dir}/.devserver_bootstrap_complete/" ],
+      require  => Vcsrepo["$install_dir"],
     }
-
     $msg = "    This file is a marker to stop puppet Exec from
     re-running build/run bootstrap. Puppet will not re-run
     build/run bootstrap if this file exists."
-    file { "${install_dir}/.devserver_bootstrap_has_run":
+    file { "${install_dir}/.devserver_bootstrap_complete":
       ensure  => present,
       content => $msg,
       owner   => $user,
       group   => $user,
     }
+    # db_url should be added in place to the config-default.rb file with
+    # ini_file.  But that's the next step
+    file {"${install_dir}/common/config/config-defaults.rb" :
+      ensure  => present,
+      owner   => $user,
+      group   => $user,
+      source  => "puppet:///modules/archivesspace_dev/config-defaults.rb",
+      require => Vcsrepo["$install_dir"],
+      notify  => Exec['build/run db:migrate'],
+    }
+    exec { 'build/run db:migrate' :
+      cwd      => $install_dir,
+      command  => "${install_dir}/build/run db:migrate",
+      timeout  => 1800,
+      creates  => "${install_dir}/.devserver_db_migrate_complete",
+      notify   => File [ "${install_dir}/.devserver_db_migrate_complete" ],
+      require  => [Remote_file["${install_dir}/common/lib/mysql-connector-java-5.1.34.jar"], Exec['build/run bootstrap']],
+    }
+    file { "${install_dir}/.devserver_db_migrate_complete":
+      ensure  => present,
+      content => "Database migration complete.",
+      owner   => $user,
+      group   => $user,
+    }
+
 
 }
